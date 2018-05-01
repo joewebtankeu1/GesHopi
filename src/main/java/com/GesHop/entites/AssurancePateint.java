4b5379594f7c58b8def707568871219c734754c3
joewebtankeu1 <tchounkeujoel@gmail.com>
2018-05-01 08:20:41 +0100

    GesHospi01/05/18

--- src/main/java/com/GesHop/entites/AssurancePateint.java
+++ src/main/java/com/GesHop/entites/AssurancePateint.java
@@ -17,11 +17,75 @@ public class AssurancePateint implements Serializable{
 	private Long NumSecurite;
 	private String Numcontrat;
 	private Long isAyantDroit;
-	@ManyToOne
-	@JoinColumn(name="NumCentralPatient")
 	private String NomPrenomAssure; 
 	private Date Datecreation;  
+	@ManyToOne
+	@JoinColumn(name="NumCentralPatient")
 	private Pateint patient;
-	private Long CodeTiers;
+	@ManyToOne
+	@JoinColumn(name="CodeTiers")
+	private Tiers tiers;
+	public AssurancePateint() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public AssurancePateint(Long numAssure, Long numSecurite, String numcontrat, Long isAyantDroit,
+			String nomPrenomAssure, Date datecreation) {
+		super();
+		NumAssure = numAssure;
+		NumSecurite = numSecurite;
+		Numcontrat = numcontrat;
+		this.isAyantDroit = isAyantDroit;
+		NomPrenomAssure = nomPrenomAssure;
+		Datecreation = datecreation;
+	}
+	public Long getNumAssure() {
+		return NumAssure;
+	}
+	public void setNumAssure(Long numAssure) {
+		NumAssure = numAssure;
+	}
+	public Long getNumSecurite() {
+		return NumSecurite;
+	}
+	public void setNumSecurite(Long numSecurite) {
+		NumSecurite = numSecurite;
+	}
+	public String getNumcontrat() {
+		return Numcontrat;
+	}
+	public void setNumcontrat(String numcontrat) {
+		Numcontrat = numcontrat;
+	}
+	public Long getIsAyantDroit() {
+		return isAyantDroit;
+	}
+	public void setIsAyantDroit(Long isAyantDroit) {
+		this.isAyantDroit = isAyantDroit;
+	}
+	public String getNomPrenomAssure() {
+		return NomPrenomAssure;
+	}
+	public void setNomPrenomAssure(String nomPrenomAssure) {
+		NomPrenomAssure = nomPrenomAssure;
+	}
+	public Date getDatecreation() {
+		return Datecreation;
+	}
+	public void setDatecreation(Date datecreation) {
+		Datecreation = datecreation;
+	}
+	public Pateint getPatient() {
+		return patient;
+	}
+	public void setPatient(Pateint patient) {
+		this.patient = patient;
+	}
+	public Tiers getTiers() {
+		return tiers;
+	}
+	public void setTiers(Tiers tiers) {
+		this.tiers = tiers;
+	}
 
 }
--- src/main/java/com/GesHop/entites/Pateint.java
+++ src/main/java/com/GesHop/entites/Pateint.java
@@ -1,5 +1,27 @@
 package com.GesHop.entites;
 
-public class Pateint {
+import java.io.Serializable;
+import java.util.Date;
+
+public class Pateint implements Serializable {
+	private Long NumCentralPatient; 
+	private String NomPateint; 
+	private String PrenomPatient;
+	private Date DateNaisPatient;
+	private String  LieuNaisPatient;
+	private Long SexePatient;
+	IdCoordonnees, 
+	private Civilite CodeCivilite;
+	private String EmprunteDigid;
+	private String SigneBiometrie;
+	private Date DatesCreationPatient;
+	private String CommantairePatient;
+	private String NomPrenomPatient;
+	private String NomPatientMask;
+	private String CodeBarrePatient; 
+	private TypePatient typePatient; 
+	private Groupe groupePatient; 
+	private Profession IdProfession;
+	private Long IsInfoACompleter;
 
 }
